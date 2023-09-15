0367757d896fc5026bc8e19b41ee5de76d63af01
wlsdn2749 <wlsdn2749@naver.com>
2023-09-16 03:42:52 +0900

    first commit

---
+++ games/hellopygame.py
@@ -0,0 +1,18 @@
+import pygame
+import os
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 840), 0, 32)
+pygame.display.set_caption("Hello Pygame")
+screen.fill((0, 0, 0))
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT:
+            game_over = True
+            
+            
+pygame.quit()
+            
---
+++ tests/mypy_example.py
@@ -0,0 +1,11 @@
+# def greet(name: str) -> None:
+#     print("Hello, " + name)
+    
+
+# greet(42)
+
+# def repeat(message: str, times: int = 2) -> list:
+#     return [message] * times
+
+
+# repeat("Hi", "3")
