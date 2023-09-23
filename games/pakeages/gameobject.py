1ecd2c0659203753f4d3c19e4b7d03046bc82cbc
wlsdn2749 <wlsdn2749@gmail.com>
2023-09-23 12:24:39 +0900

    split: element info from gameobject.py into element.py

--- games/pakeages/gameobject.py
+++ games/pakeages/gameobject.py
@@ -1,5 +1,6 @@
 import pygame
 from typing import Any
+from pakeages.element import ThunderStroke, WaterSpout  # noqa: E402
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -8,12 +9,6 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 DARK_RED = (150, 30, 30)
 TILE1 = (0, 51, 102)
-
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
-diagonal_x = [-1, 1, -1, 1]  # 대각선 x : 1, 5, 7, 11
-diagonal_y = [-1, 1, 1, -1]  # 대각선 y : 1, 5, 7, 11
 
 pygame.font.init()
 font = pygame.font.Font("./games/fonts/HeirofLightRegular.ttf", 25)
@@ -51,53 +46,6 @@ class Tile(pygame.Rect):  # type: ignore
 
         surface.blit(text_surface, (text_x, text_y))
         # CHATGPT_END
-
-
-class Element:
-    def __init__(self) -> None:
-        pass
-
-    def get_image(self, path: str) -> pygame.Surface:
-        self.image = pygame.image.load(path)
-        return self.image
-
-    def transform(self, width: int, height: int) -> None:
-        self.image = pygame.transform.scale(self.image, (width, height))
-
-
-class ThunderStroke(Element):
-    """
-    - 등급 : 일반
-    - 효과 : 선택한 석판 100% **타격**, 가로 세로 1칸 50% **타격**
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.image = self.get_image("./games/images/thunderstroke.jpg")
-
-    def effect_range(self, x: int, y: int) -> list[tuple[int, int, int]]:
-        range_list = list()
-        range_list.append((x, y, 100))  # 상하좌우 100%
-        for i in range(4):
-            range_list.append((x + dx[i], y + dy[i], 50))  # 상하좌우 한칸씩 50%
-
-        return range_list
-
-
-class WaterSpout(Element):
-    def __init__(self) -> None:
-        super().__init__()
-        self.image = self.get_image("./games/images/waterspout.png")
-
-    def effect_range(self, y: int, x: int) -> list[tuple[int, int, int]]:
-        range_list = list()
-        range_list.append((y, x, 100))  # 상하좌우 100%
-        for i in range(4):
-            range_list.append(
-                (y + diagonal_y[i], x + diagonal_x[i], 50)
-            )  # 상하좌우 한칸씩 50%
-
-        return range_list
 
 
 class Card:
