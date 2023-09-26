8d71b4894278c8473561ea1ceea82f033c836c3c
wlsdn2749 <wlsdn2749@gmail.com>
2023-09-26 17:03:52 +0900

    Add: Hellfire elements

--- games/pakeages/element.py
+++ games/pakeages/element.py
@@ -2,11 +2,11 @@ from typing import Any
 import pygame
 import inspect
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+dx = [0, 1, 0, -1]
 
-diagonal_x = [-1, 1, -1, 1]  # 대각선 x : 1, 5, 7, 11
-diagonal_y = [-1, 1, 1, -1]  # 대각선 y : 1, 5, 7, 11
+diagonal_y = [-1, 1, -1, 1]  # 대각선 x : 1, 5, 7, 11
+diagonal_x = [-1, 1, 1, -1]  # 대각선 y : 1, 5, 7, 11
 
 
 class Element:
@@ -41,17 +41,47 @@ class ThunderStroke(Element):
 
 
 class WaterSpout(Element):
+    """
+    - 등급 : 일반
+    - 효과 : 선택한 석판 100% **타격**, 대각선 1칸 50% **타격**
+    """
+
     def __init__(self) -> None:
         super().__init__()
         self.image = self.get_image("./games/images/waterspout.png")
 
-    def effect_range(self, y: int, x: int) -> list[tuple[int, int, int]]:
+    def effect_range(self, x: int, y: int) -> list[tuple[int, int, int]]:
         range_list = list()
-        range_list.append((y, x, 100))  # 상하좌우 100%
+        range_list.append((x, y, 100))  # 상하좌우 100%
         for i in range(4):
-            range_list.append(
-                (y + diagonal_y[i], x + diagonal_x[i], 50)
-            )  # 상하좌우 한칸씩 50%
+            range_list.append((x + diagonal_x[i], y + diagonal_y[i], 50))
+            # 대각선 한칸씩 50%
+
+        return range_list
+
+
+class HellFire(Element):
+    """
+    - 등급 : 일반
+    - 효과 : 선택한 석판 100% **타격**, 대각선 1칸 50% **타격** 가로 세로 2칸씩 50% 타격
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.image = self.get_image("./games/images/hellfire.jpg")
+
+    def effect_range(self, x: int, y: int) -> list[tuple[int, int, int]]:
+        range_list = list()
+        range_list.append((x, y, 100))  # 상하좌우 100%
+        for i in range(4):
+            range_list.append((x + diagonal_x[i], y + diagonal_y[i], 50))
+            # 대각선 한칸씩 50%
+
+            range_list.append((x + dx[i], y + dy[i], 50))
+            # 상하좌우 한칸씩 50%
+
+            range_list.append((x + 2 * dx[i], y + 2 * dy[i], 50))
+            # 상하좌우 두칸씩 50%
 
         return range_list
 
